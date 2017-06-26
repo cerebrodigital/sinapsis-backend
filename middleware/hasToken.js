0002bd42978145156384998ac6f88558f28d6658
adair <yadaxim@gmail.com>
2017-06-26 13:01:04 -0500

    no console.log

--- middleware/hasToken.js
+++ middleware/hasToken.js
@@ -1,4 +1,3 @@
-
 module.exports = function(opts){
   return function (req,res,next){
 
@@ -9,11 +8,9 @@ module.exports = function(opts){
         if(err){
           return res.status(401).json({message: 'bad token'})
         }
-        console.log("decoded", decoded)
         User.findById(decoded.id)
         .catch(err=>{ return res.stataus(401).json({message: 'bad token'})})
         .then(found=>{
-          console.log('found user', found)
           req.user = found
           next()
         })
