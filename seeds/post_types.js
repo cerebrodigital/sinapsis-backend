af360e09d8571cca297b18791606fd0a09de1a99
esteban <estecaes@gmail.com>
2017-06-28 10:48:26 -0500

    safely populate

--- seeds/post_types.js
+++ seeds/post_types.js
@@ -1,13 +1,16 @@
 'use strict';
 var uuid = require('node-uuid');
 module.exports = function(models) {
-  models.PostType.bulkCreate([
-    {id:uuid.v4(), code: 'img', name: 'Image'},
-    {id:uuid.v4(), code: 'link', name: 'Link'},
-    {id:uuid.v4(), code: 'article', name: 'Article'},
-    {id:uuid.v4(), code: 'video', name: 'Video'}
-    ])
-  .spread((affectedCount, affectedRows)=>{
-      console.log('PostType created')
+  models.PostType.destroy({where: {}, truncate: true})
+  .then(()=>{
+    models.PostType.bulkCreate([
+      {id:uuid.v4(), code: 'img', name: 'Image'},
+      {id:uuid.v4(), code: 'link', name: 'Link'},
+      {id:uuid.v4(), code: 'article', name: 'Article'},
+      {id:uuid.v4(), code: 'video', name: 'Video'}
+      ])
+    .spread((affectedCount, affectedRows)=>{
+        console.log('PostType created')
+    })
   })
 }
--- seeds/vote_types.js
+++ seeds/vote_types.js
@@ -1,11 +1,14 @@
 'use strict';
 var uuid = require('node-uuid');
 module.exports = function(models) {
-  models.VoteType.bulkCreate([
-    {id:uuid.v4(), code: 'up', name: 'Up'},
-    {id:uuid.v4(), code: 'down', name: 'Down'},
-    ])
-  .spread((affectedCount, affectedRows)=>{
-      console.log('VoteType created')
+  models.VoteType.destroy({where: {}, truncate: true})
+  .then(()=>{
+    models.VoteType.bulkCreate([
+      {id:uuid.v4(), code: 'up', name: 'Up'},
+      {id:uuid.v4(), code: 'down', name: 'Down'},
+      ])
+    .spread((affectedCount, affectedRows)=>{
+        console.log('VoteType created')
+    })
   })
 }
