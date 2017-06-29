c3c926bd31f75757b53d3ab0b89b383f11360f74
adair <yadaxim@gmail.com>
2017-06-29 14:18:28 -0500

    out of place file

--- seeds/action_types.js
+++ seeds/action_types.js
@@ -4,6 +4,7 @@ module.exports = function(models) {
   models.ActionType.destroy({where: {}, truncate: true})
   .then(()=>{
     models.ActionType.bulkCreate([
+      {id:uuid.v4(), code: 'profileUpdated', name: 'Profile Updated'}
       {id:uuid.v4(), code: 'comment', name: 'Comment'},
       {id:uuid.v4(), code: 'post', name: 'Post'},
       {id:uuid.v4(), code: 'upVote', name: 'Up Vote'},
