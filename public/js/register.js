df5803303aa25f89132a19f84607e37cf5141fe5
adair <yadaxim@gmail.com>
2017-07-04 10:16:51 -0500

    register without ajax

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,14 @@
 $(($)=>{
   console.log('Registrando el usuario')
-  $('#register').submit(function(e){
-    e.preventDefault();
-    console.log("Registrando");
-    data = $('#register').serialize();
-    console.log(data);
-    $.post('http://localhost:3003/auth/register', data, function(res) {
-      console.log("dando click en registrar", res);
-    });
+  // $('#register').submit(function(e){
+  //   e.preventDefault();
+  //   console.log("Registrando");
+  //   data = $('#register').serialize();
+  //   console.log(data);
+  //   $.post('http://localhost:3003/auth/register', data, function(res) {
+  //     console.log("dando click en registrar", res);
+  //   });
 
-  })
+  // })
 
 })
